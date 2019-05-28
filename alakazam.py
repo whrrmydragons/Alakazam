96230066a9ee3ef1c20327b3adaa46b08d5994b6
omri haber <omrihaber@me.com>
2019-05-28 23:18:34 +0300

    @refactor/added comments

--- alakazam.py
+++ alakazam.py
@@ -10,14 +10,24 @@ expect that the request contains a field name data which is an array containning
 '''
 @app.route("/", methods=['POST'])
 def predict():
+    #get the parsed request`s body
     body = request.get_json(force=True)
+    #get the data parameter from the body
     data = body['data']
+    #get the periods parameter from the body
+    #if its undefined its default value will be 365 steps 
     periods = body['periods'] if 'periods' in body else 365
+    #turn the data recieved into a dataframe
     df = pd.io.json.json_normalize(data)
+    #instinciate model
     m = Prophet()
+    #train/fit the model
     m.fit(df)
+    #make a dataframe of future dates whos prediction we want to return to the user
     future = m.make_future_dataframe(periods=periods)[-periods:]
+    #calculate future predictions a.k.a forecast
     forecast = m.predict(future)
+    #return the forecast/prediction
     return forecast.to_json(orient='records')
 
 
